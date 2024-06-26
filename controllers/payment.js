3f0c49027d6b8d6a36aff6d273c36767ac116257
shivendra <shivendrakeshari581@gmail.com>
2024-06-27 00:07:48 +0530

    add payment method

--- controllers/payment.js
+++ controllers/payment.js
@@ -34,10 +34,10 @@ if(!course){
              message:"Could not find the course related to id "
 
     });
-    // user already pay for the same id :-
+    // user already pay for the same id :- user id in the user schema is present in the form of object id and the request com
 
     const uid=new mongoose.Types.Object.Id(userid);
-    if(course.studentsEnrolled.includes(uid)){
+    if(course.StudentEnrolled.includes(uid)){
         return res.status(401).json({
             success:false,
             message:"User already enrolled"
@@ -56,7 +56,80 @@ if(!course){
 
 
     // order create
+    const amount=course.price;
+    const currency="INR"
+
+    // return response.status(200).json({
+
+    // })
+
+    const option={
+
+        amount: amount*100,
+        currency,
+        recipt : Math.random(Date.now()).toString(),
+        notes:{
+            courseId:course_id,
+            userid,
+
+        }
+
+    };
+
+    try{
+        // initiate the payment using razorpay 
+        const paymentresponse=await instance.order.create(option);
+        console.log(paymentresponse);
+
+        return res.status(200).json({
+            success:true,
+      coursename:course.coursename,
+      coursedescription:course.thumbnail,
+      orderId:paymentresponse.id,
+      currency:paymentresponse.currency,
+      amount:paymentresponse.amount
+
+        })
+    }
+    catch(error){
+        console.log(error);
+        return res.json({
+            success:false,
+            message:"could not initiate order"
+        });
+    }
+    
 
     // return response 
 
+
+
+}
+
+// verify signature
+
+exports.verifySignature=async (req,res)=>{
+   const webhooksceret="1234567";
+
+   const signature=req.headers["x-razorpay-signature"];
+   const shasum=crypto.createHamc("sha256",webhooksceret);
+
+   shasum.update(JSON.stringify(req.body));
+
+   const digest=shasum.digest("hex");
+
+   if(signature === digest){
+    console.log("payment is autrhorized");
+
+    const {courseid,userid}=req.body.payload.entity.notes;
+    
+
+
+
+
+
+
+
+
+   }
 }
